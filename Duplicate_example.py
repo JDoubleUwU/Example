40c79a25e32e37492a985a32127ceb5ce7f9e1a3
JDoubleUwU <125931177+JDoubleUwU@users.noreply.github.com>
2023-10-30 21:39:34 -0700

    Add files via upload

--- Duplicate_example.py
+++ Duplicate_example.py
@@ -10,12 +10,14 @@ def calculate_perimeter(length, width):
 
 
 def calculate_volume(length, width, height):
-    volume = length * width * height
+    #volume = length * width * height
+    volume = calculate_area(length, width) * height
     return volume
 
 
 def calculate_surface_area(length, width, height):
-    surface_area = 2 * (length * width + length * height + width * height)
+    #surface_area = 2 * (length * width + length * height + width * height)
+    surface_area = 2 * (calculate_area(length, width) + length * height + width * height)
     return surface_area
 
 
